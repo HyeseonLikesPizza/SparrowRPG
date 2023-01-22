0a45c36c1ebeb486fbc6c7d609472160787748b2
HyeSeon <1725f0701@gmail.com>
2023-01-22 13:10:08 +0900

    230122 Enemy Patrol

--- Source/SparrowRPG/SparrowRPG.Build.cs
+++ Source/SparrowRPG/SparrowRPG.Build.cs
@@ -8,7 +8,7 @@ public class SparrowRPG : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "HairStrandsCore", "GeometryCollectionEngine", "Niagara", "UMG"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "HairStrandsCore", "GeometryCollectionEngine", "Niagara", "UMG", "AIModule"});
 	}
 }
  
